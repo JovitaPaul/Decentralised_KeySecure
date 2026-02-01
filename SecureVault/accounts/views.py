3403202e2380e67057461329be3c3a3630970549
JovitaPaul <jovita.joby@gmail.com>
2026-02-01 13:38:36 +0530

    Fixed login and signup pages with backend

--- SecureVault/accounts/views.py
+++ SecureVault/accounts/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 def login_view(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("upload_key")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {"form": form})
 
 
 def signup_view(request):
-    return render(request, 'signup.html')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect("upload_key")
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {"form": form})
 
 
 def profile_view(request):
